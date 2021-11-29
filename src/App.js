397081ce4f8e1fa56b89ab68f9390f1ee91163c2
Mehammed <checoslbches@gmail.com>
2021-11-29 21:33:52 +0300

    completed image gallery app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
+  const [images, setImages]=useState([]);
+  const [isLoading,setIsLoading]= useState(true);
+  const [term, setTerm]= useState('');
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+        .then(res=>res.json())
+        .then(data=>{
+          setImages(data.hits);
+          setIsLoading(false);
+        })
+        .catch(err=>console.log(err));
+  },[term]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="px-6 py-4 bg-gray-700 align-middle justify-center">
+        <div className="text-6xl font-bold text-blue-500 text-center mb-2">Image Gallery with Pixabay api </div>
+     </header>
+     <hr />
+     <ImageSearch searchText={(text)=>setTerm(text)} />
+     <main className="container mx-auto">
+       {/* checking if image is found */}
+       {!isLoading && images.length ===0 && <h1 className="text-6xl text-center mx-auto mt-32">NO image Found</h1>}
+       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+       :<div className="grid grid-cols-3 gab-4">
+          {images.map(image=>(
+             <ImageCard key={image.id} image={image} />
+          ))}
+     </div>}
+       
+     </main>
+     <hr />
+      <footer className="px-6 py-4 bg-gray-700 align-middle justify-center">
+         <div className="text-xl text-center py-4  text-blue-500 mb-2">made with love by <a href="https://twitter.com/checoslbches" target="_blank"><span className="text-green-500 text-4xl" >Mehammed Teshome</span></a></div>
+      </footer>
     </div>
   );
 }
